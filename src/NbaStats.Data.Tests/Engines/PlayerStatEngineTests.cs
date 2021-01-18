74b14fc4f28ae48ee47ebffbf56b20a53fb1c460
Larry Shields <larry.shields@gmail.com>
2021-01-18 14:57:19 -0500

    Added Player Stat Engine with Tests

---
+++ src/NbaStats.Data.Tests/Engines/PlayerStatEngineTests.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NbaStats.Data.Engines;
+using NbaStats.Data.Context;
+using NbaStats.Data.Repositories;
+using NbaStats.Data.DataObjects;
+using NUnit.Framework;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace NbaStats.Data.Tests.Engines
+{
+    [TestFixture]
+    public class PlayerStatEngineTests
+    {
+        private IStatEngine<PlayerStat> engine;
+        private SqlContext ctx;
+        private PlayerStat stat;
+
+        [SetUp]
+        public void Setup()
+        {
+            ctx = new SqlContext();
+            engine = new PlayerStatEngine(ctx);
+
+            stat = new PlayerStat()
+            {
+                Id = 1,
+                PlayerId = 1,
+                ScheduleId = 4,
+                Assists = 5,
+                PointDifferential = -12,
+                Points = 25,
+                FGPercentage = 0.25,
+                ThreePercentage = 0.15,
+                ThreeCompleted = 3,
+                ThreeTaken = 15,
+                Turnovers = 3,
+                FGTaken = 20,
+                FGCompleted = 5,
+                Fouls = 2,
+                Blocks = 5,
+                DefensiveRebound = 3,
+                OffensiveRebound = 3,
+                GameMinutes = 18,
+                Steals = 4
+            };
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            foreach(PlayerStat item in ctx.PlayerStats)
+            {
+                ctx.PlayerStats.Remove(item);
+            }
+            ctx.SaveChanges();
+        }
+
+        [Test]
+        public void TestSave()
+        {
+            engine.Save(stat);
+
+            Assert.AreEqual(1, ctx.PlayerStats.Count());
+        }
+
+        [Test]
+        public void TestSaveUpdate()
+        {
+            engine.Save(stat);
+
+            PlayerStat stat2 = new PlayerStat()
+            {
+                Id = 1,
+                PlayerId = 1,
+                ScheduleId = 4,
+                Assists = 20,
+                PointDifferential = -12,
+                Points = 25,
+                FGPercentage = 0.25,
+                ThreePercentage = 0.15,
+                ThreeCompleted = 3,
+                ThreeTaken = 15,
+                Turnovers = 3,
+                FGTaken = 20,
+                FGCompleted = 5,
+                Fouls = 2,
+                Blocks = 5,
+                DefensiveRebound = 3,
+                OffensiveRebound = 3,
+                GameMinutes = 18,
+                Steals = 4
+            };
+            engine.Save(stat2);
+
+            Assert.AreEqual(1, ctx.PlayerStats.Count());
+
+            Assert.IsNotNull(ctx.PlayerStats.Where(c => c.Id == 1 && c.Assists == 20));
+        }
+
+        [Test]
+        public void TestSaveUpdateNoId()
+        {
+            engine.Save(stat);
+
+            PlayerStat stat2 = new PlayerStat()
+            {
+                Id = 0,
+                PlayerId = 1,
+                ScheduleId = 4,
+                Assists = 20,
+                PointDifferential = -12,
+                Points = 25,
+                FGPercentage = 0.25,
+                ThreePercentage = 0.15,
+                ThreeCompleted = 3,
+                ThreeTaken = 15,
+                Turnovers = 3,
+                FGTaken = 20,
+                FGCompleted = 5,
+                Fouls = 2,
+                Blocks = 5,
+                DefensiveRebound = 3,
+                OffensiveRebound = 3,
+                GameMinutes = 18,
+                Steals = 4
+            };
+            engine.Save(stat2);
+
+            Assert.AreEqual(1, ctx.PlayerStats.Count());
+
+            Assert.IsNotNull(ctx.PlayerStats.Where(c => c.Id == 1 && c.Assists == 20));
+        }
+
+        [Test]
+        public void TestLoad()
+        {
+            engine.Save(stat);
+            Assert.AreEqual(1, ctx.PlayerStats.Count());
+            Assert.IsNotNull(ctx.PlayerStats.Where(c => c.Id == 1).FirstOrDefault());
+        }
+
+        [Test]
+        public void TestLoadAll()
+        {
+            engine.Save(stat);
+            Assert.AreEqual(1, engine.LoadAll().Count());
+        }
+
+        [Test]
+        public void TestQuery()
+        {
+            engine.Save(stat);
+            Expression<Func<PlayerStat, bool>> expression = c => c.PlayerId == 1;
+            Assert.AreEqual(1, engine.Query(expression).Count());
+        }
+
+        [Test]
+        public void TestDelete()
+        {
+            engine.Save(stat);
+            Assert.IsNotNull(ctx.PlayerStats.Where(c => c.Id == 1).FirstOrDefault());
+
+            engine.Delete(stat);
+            Assert.IsNull(ctx.PlayerStats.Where(c => c.Id == 1).FirstOrDefault());
+        }
+    }
+}
---
+++ src/NbaStats.Data/Engines/PlayerStatEngine.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NbaStats.Data.DataObjects;
+using NbaStats.Data.Context;
+using NbaStats.Data.Repositories;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace NbaStats.Data.Engines
+{
+    public class PlayerStatEngine : IStatEngine<PlayerStat>
+    {
+        private readonly IRepository repo;
+
+        /// <summary>
+        /// Creates a Player Stat Engine with an InMemory Datastore.
+        /// </summary>
+        public PlayerStatEngine()
+        {
+            repo = new SqlRepository();
+        }
+
+        /// <summary>
+        /// Creates a Player Stat Engine utilizing the provided Repository.
+        /// </summary>
+        /// <param name="repo">Repository</param>
+        public PlayerStatEngine(IRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        /// <summary>
+        /// Creates a Player Stat Engine utilizing the provide Sql Context.
+        /// </summary>
+        /// <param name="ctx">Sql Context</param>
+        public PlayerStatEngine(SqlContext ctx)
+        {
+            repo = new SqlRepository(ctx);
+        }
+
+
+        public void Delete(PlayerStat entity)
+        {
+            repo.Delete(entity);
+        }
+
+        public bool Exists(PlayerStat entity)
+        {
+            return repo.Exists(entity);
+        }
+
+        public PlayerStat Load(long id)
+        {
+            return repo.GetPlayerStat(id);
+        }
+
+        public IQueryable<PlayerStat> LoadAll()
+        {
+            return repo.GetPlayerStats();
+        }
+
+        public IQueryable<PlayerStat> Query(Expression<Func<PlayerStat, bool>> expression)
+        {
+            return repo.GetPlayerStats().Where(expression);
+        }
+
+        public PlayerStat Save(PlayerStat entity)
+        {
+            if (entity.Id > 0 && Exists(entity))
+            {
+                return repo.Update(entity);
+            }
+            else
+            {
+                if (Exists(entity))
+                {
+                    var original = repo.GetPlayerStats().Where(c => c.PlayerId == entity.PlayerId && c.ScheduleId == entity.ScheduleId).FirstOrDefault();
+                    if (original != null)
+                    {
+                        entity.Id = original.Id;
+                        return Save(entity);                            
+                    }
+                    else
+                    {
+                        return repo.Insert(entity);
+                    }
+                }
+                else
+                {
+                    return repo.Insert(entity);
+                }
+            }
+        }
+    }
+}
